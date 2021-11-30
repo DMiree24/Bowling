a2e9c3e8b5fdc876bc060331713ac657b9432c47
Durrell Miree <dmiree@ltu.edu>
2021-11-30 16:34:25 -0500

    Initial commit

---
+++ Assets/Scripts/Contact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Contact : MonoBehaviour
+{
+    public AudioSource pinsAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "BowlingBall")
+        {
+            pinsAudio.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Rolling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour
+{
+    public float speed = 2;
+    private Rigidbody rb;
+
+    //Use this for initialization 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float movementHorizontal = Input.GetAxis ("Horizontal");
+        float movementVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+        rb.AddForce(movement * speed); 
+    }
+}
